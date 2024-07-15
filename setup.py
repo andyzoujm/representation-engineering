0f6dd103efacbf97eb20a49d695eaef036746df2
Justin Wang <justinwang46@gmail.com>
2024-07-15 09:45:56 +0000

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_namespace_packages
+import setuptools
 
-setup(
-    name="repe",
-    version="0.1",
-    description="",
-    packages=find_namespace_packages(),  # This will automatically find packages
-    author="Center for AI Safety",
-    author_email="",
-    url="https://github.com/andyzoujm/representation-engineering",
-    install_requires=[
-        "transformers",
-        "accelerate",
-    ],
-)
+setuptools.setup()
